b22fdec5e007998103366c6392c083b04c2d9b76
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-02-24 07:58:59 -0800

    add ExceptionControllerAdvice

---
+++ src/main/java/com/learn/gsws_ch10_ex1/ErrorDetails.java
@@ -0,0 +1,14 @@
+package com.learn.gsws_ch10_ex1;
+
+public class ErrorDetails {
+
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
