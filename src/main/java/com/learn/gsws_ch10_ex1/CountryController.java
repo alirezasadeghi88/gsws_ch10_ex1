3f91b1cbce1b2a973540588a221d6d48b28daaf2
alirezasadeghi88 <alirezasadeghi0558@gmail.com>
2026-02-24 07:55:05 -0800

    add PaymentDetails

--- src/main/java/com/learn/gsws_ch10_ex1/CountryController.java
+++ src/main/java/com/learn/gsws_ch10_ex1/CountryController.java
@@ -1,5 +1,7 @@
 package com.learn.gsws_ch10_ex1;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,16 +11,14 @@ import java.util.List;
 public class CountryController {
 
     @GetMapping("/france")
-    public Country france() {
+    public ResponseEntity<Country> france() {
         Country c = Country.of("France", 67);
-        return c;
-    }
-
-    @GetMapping("/all")
-    public List<Country> countries() {
-        Country c1 = Country.of("France", 67);
-        Country c2 = Country.of("Spain", 47);
-        return List.of(c1,c2);
+        return ResponseEntity
+                .status(HttpStatus.ACCEPTED)
+.header("continent", "Europe")
+.header("capital", "Paris")
+.header("favorite_food", "cheese and wine")
+.body(c);
     }
 
 }
